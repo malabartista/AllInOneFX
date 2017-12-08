6258a45d8e3955b61d768129ef8c1f4289c8343c
malabartista <elchampion@gmail.com>
2017-12-08 10:54:19 +0100

    Configs

--- src/main/java/com/allinonefx/config/Configs.java
+++ src/main/java/com/allinonefx/config/Configs.java
@@ -1,10 +1,9 @@
 package com.allinonefx.config;
 
 public class Configs {
-
     protected static String dbhost = "localhost";
     protected static String dbport = "3306";
     protected static String dbuser = "root";
-    protected static String dbpass = "Siga1234$";
-    protected static String dbname = "studentfx";
+    protected static String dbpass = "";
+    protected static String dbname = "allinonefx";
 }
